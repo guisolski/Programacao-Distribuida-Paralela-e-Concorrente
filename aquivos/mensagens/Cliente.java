e439af6f1111ec9bbc9aaa7c6a07b190ec35bd27
Guilherme Solski Alves <guigusolski@gmail.com>
2019-11-12 12:41:23 -0300

    Update Cliente.java

--- aquivos/mensagens/Cliente.java
+++ aquivos/mensagens/Cliente.java
@@ -1,20 +1,13 @@
-﻿import java.io.BufferedReader;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.net.Inet4Address;
 import java.net.Socket;
 
+public class Cliente {
 
-public class Cliente extends Thread {
-    // parte que controla a recepção de mensagens do cliente
-    private Socket conexao;
-    // construtor que recebe o socket do cliente
-    public Cliente(Socket socket) {
-        this.conexao = socket;
-    }
-    public static void main(String args[])
-    {
+    public static void main(String args[]) {
         try {
             //Instancia do atributo conexao do tipo Socket,
             // conecta a IP do Servidor, Porta
@@ -30,51 +23,13 @@ public class Cliente extends Thread {
             //envia o ip do usuario
             saida.println(Inet4Address.getLocalHost().getHostAddress());
             //instancia a thread para ip e porta conectados e depois inicia ela
-            Thread thread = new Cliente(socket);
+            Thread thread = new ClientPega(socket);
             thread.start();
-            //Cria a variavel msg responsavel por enviar a mensagem para o servidor
-            String msg;
-            while (true)
-            {
-                // cria linha para digitação da mensagem e a armazena na variavel msg
-                System.out.print("Ação: ");
-                msg = teclado.readLine();
-                // envia a mensagem para o servidor
-                saida.println(msg);
-            }
+            thread = new ClienteEvia(teclado, saida);
+            thread.start();
         } catch (IOException e) {
             System.out.println("Falha na Conexao... .. ." + " IOException: " + e);
         }
     }
-    // execução da thread
-    public void run()
-    {
-        try {
-            //recebe mensagens de outro cliente através do servidor
-            BufferedReader entrada =
-                    new BufferedReader(new InputStreamReader(this.conexao.getInputStream()));
-            //cria variavel de mensagem
-            String msg;
-            while (true)
-            {
-                // pega o que o servidor enviou
-                msg = entrada.readLine();
-                //se a mensagem contiver dados, passa pelo if,
-                // caso contrario cai no break e encerra a conexao
-                if (msg == null) {
-                    System.out.println("Conexão encerrada!");
-                    System.exit(0);
-                }
-                System.out.println();
-                //imprime a mensagem recebida
-                System.out.println(msg);
-                //cria uma linha visual para resposta
-                System.out.print("Ação: ");
-            }
-        } catch (IOException e) {
-            // caso ocorra alguma exceção de E/S, mostra qual foi.
-            System.out.println("Ocorreu uma Falha... .. ." +
-                    " IOException: " + e);
-        }
-    }
+
 }
