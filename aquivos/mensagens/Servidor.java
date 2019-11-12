796e705276cbca8ee90e04bcc804815055074a16
Guilherme Solski Alves <guigusolski@gmail.com>
2019-11-12 12:42:11 -0300

    Update Servidor.java

--- aquivos/mensagens/Servidor.java
+++ aquivos/mensagens/Servidor.java
@@ -1,18 +1,15 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+
 
 
 public class Servidor extends Thread {
     private static HashMap<String, PrintStream> MAP_CLIENTES;
-    private Socket conexao;
+    private static  Socket conexao;
     private static ArrayList<String> LISTA_DE_NOMES = new ArrayList<>();
     private static ArrayList<String> LISTA_DE_IP = new ArrayList<>();
     private static HashMap<String, String> CLIENTE_MENSAGEM;
@@ -24,8 +21,8 @@ public class Servidor extends Thread {
             ServerSocket server = new ServerSocket(5555);
             System.out.println("ServidorSocket rodando na porta 5555");
             while (true) {
-                Socket conexao = server.accept();
-                Thread t = new ServeJob(conexao,LISTA_DE_NOMES,LISTA_DE_IP, MAP_CLIENTES,CLIENTE_MENSAGEM);
+                conexao = server.accept();
+                Thread t = new ServerJob(conexao,LISTA_DE_NOMES,LISTA_DE_IP, MAP_CLIENTES,CLIENTE_MENSAGEM);
                 t.start();
             }
         } catch (IOException e) {
